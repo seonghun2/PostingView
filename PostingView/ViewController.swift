0a36cb43868c120a42061551eb15e6ed300eaf32
seonghun2 <tjdgnsok2@naver.com>
2023-01-30 14:08:49 +0900

    Initial Commit

---
+++ PostingView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostingView
+//
+//  Created by user on 2023/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
